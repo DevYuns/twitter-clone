93c9c9f3ff819196383b3b04619400e3b714ae16
DevYuns <ydhun93@gmail.com>
2021-03-08 20:11:37 +0900

    Add firebase setting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from './firebase';
+
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
