1dbf4a0ebce1a980c67a3c84a9f5f60d145e6fb8
DevYuns <ydhun93@gmail.com>
2021-03-09 14:16:02 +0900

    Make basic auth form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onChange = (e) => {
+    const {
+      target: {
+        name, value
+      }
+    } = e;
+    if(name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+  } 
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input 
+          name="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+        <input 
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <input type="submit" value="Log In"/>
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  )
+};
 
 export default Auth;
